cf1a1a8d525ac8edaa8385a4095117d46b55afd2
minsu45 <98883505+minsu45@users.noreply.github.com>
2022-07-04 18:41:35 +0900

    고객센터 공지사항 리스트
    공지사항 화면 구현함

--- iBlossom-Project/src/main/java/com/kh/iblossom/notice/controller/NoticeController.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/notice/controller/NoticeController.java
@@ -1,12 +1,20 @@
 package com.kh.iblossom.notice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kh.iblossom.notice.model.service.NoticeService;
+
+@Controller
 public class NoticeController {
-<<<<<<< Updated upstream
+	
+	@Autowired 
+	private NoticeService noticeService;
+	 
+	@RequestMapping("listView.no")
+	public String noticeListView() {
 
-=======
-	/*DDD*/
-
-
-	/*하이루*/
->>>>>>> Stashed changes
+		return "user/notice/notice_ListView";
+	}
 }
--- iBlossom-Project/src/main/java/com/kh/iblossom/notice/model/service/NoticeService.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/notice/model/service/NoticeService.java
@@ -1,5 +1,11 @@
 package com.kh.iblossom.notice.model.service;
 
+import java.util.ArrayList;
+
+import com.kh.iblossom.notice.model.vo.Notice;
+
 public interface NoticeService {
+	
+	ArrayList<Notice> selectList();
 
 }
