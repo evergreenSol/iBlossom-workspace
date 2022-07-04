221601bdf57f922ce11deb6deae41df3e84f2feb
daon <105149013+leedaon@users.noreply.github.com>
2022-07-04 21:05:08 +0900

    Cart, Order, DetailOrder VO클래스 생성 - 다온
    Cart, Order, DetailOrder VO 클래스 생성함

--- iBlossom-Project/src/main/java/com/kh/iblossom/cart/model/vo/Cart.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/cart/model/vo/Cart.java
@@ -1,5 +1,18 @@
 package com.kh.iblossom.cart.model.vo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cart {
 
+	private int cartNo; // CART_NO NUMBER PRIMARY KEY,
+	private int userNo; // USER_NO NUMBER NOT NULL,
+	private int productNo; // PRODUCT_NO NUMBER NOT NULL,	
+	private int productCount; // PRODUCT_COUNT NUMBER NOT NULL,
+	private int productPrice; // PRODUCT_PRICE NUMBER NOT NULL,
+	
 }
