66395cfe43c9c52bfb1b32ee5ba1cb1c82dea219
evergreenSol <107356095+evergreenSol@users.noreply.github.com>
2022-07-05 14:09:32 +0900

    Revert "views => product => ~.jsp (7개)   / product => ProductController.java 추가  - 지승아"

--- iBlossom-Project/src/main/java/com/kh/iblossom/product/model/dao/ProductDao.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/product/model/dao/ProductDao.java
@@ -1,8 +1,5 @@
 package com.kh.iblossom.product.model.dao;
 
-import org.springframework.stereotype.Repository;
-
-@Repository
 public class ProductDao {
 
 }
--- iBlossom-Project/src/main/java/com/kh/iblossom/product/model/service/ProductServiceImpl.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/product/model/service/ProductServiceImpl.java
@@ -1,8 +1,5 @@
 package com.kh.iblossom.product.model.service;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class ProductServiceImpl implements ProductService{
 
 }
