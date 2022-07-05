5a973b3c6e7ace231db165b40e31a6e363ac489e
evergreenSol <craft0429@gmail.com>
2022-07-05 14:11:41 +0900

    mypage 구현 및 images 폴더 삭제(중복)
    mypage 구현 및 images 폴더 삭제(중복) - 한솔

--- iBlossom-Project/src/main/java/com/kh/iblossom/product/model/dao/ProductDao.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/product/model/dao/ProductDao.java
@@ -1,5 +1,21 @@
 package com.kh.iblossom.product.model.dao;
 
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.kh.iblossom.product.model.vo.Review;
+
+@Repository
 public class ProductDao {
+	
+	public ArrayList<Review> selectMyReview(SqlSessionTemplate sqlSession, int userNo) {
+		
+		ArrayList<Review> list = new ArrayList<Review>();
+		
+		// return (ArrayList)sqlSession.selectList("reviewMapper.selectList", userNo);
+		return list;
+	}
 
 }
