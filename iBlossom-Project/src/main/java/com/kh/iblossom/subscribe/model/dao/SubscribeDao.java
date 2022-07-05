90bf7219138681489a1c2e7e5fdafe3db738a898
dkim62 <donghyeonk96@gmail.com>
2022-07-05 16:22:34 +0900

    2022/07/05
    2022/07/05-김동현
    
    수정사항
    1) 구독 상품 Delete 기능 추가
    2) 구독 상품 단일 조회 기능 추가
    3) JSP/CSS 업데이트

--- iBlossom-Project/src/main/java/com/kh/iblossom/subscribe/model/dao/SubscribeDao.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/subscribe/model/dao/SubscribeDao.java
@@ -9,14 +9,29 @@ import com.kh.iblossom.subscribe.model.vo.SubProduct;
 
 @Repository
 public class SubscribeDao {
-
+	
+	// 정기구독 상품 조회용 메소드
 	public ArrayList<SubProduct> selectList(SqlSessionTemplate sqlSession) {
 		
-		return (ArrayList)sqlSession.selectList("subscribeMapper.selectList");
-	}
-
+	
+	  return (ArrayList)sqlSession.selectList("subscribeMapper.selectList"); 
+	 }
+	 
+	// 정기구독 상품 추가용 메소드
 	public int insertSubProduct(SqlSessionTemplate sqlSession, SubProduct sp) {
 
 		return sqlSession.insert("subscribeMapper.insertSubProduct", sp);
+	}
+
+	// 정기구독 상품 삭제용 메소드
+	public int deleteSubProduct(SqlSessionTemplate sqlSession, int spno) {
+		
+		return sqlSession.delete("subscribeMapper.deleteSubProduct", spno);
+		
+	}
+
+	public SubProduct insertSubProduct(SqlSessionTemplate sqlSession, int spno) {
+		
+		return sqlSession.selectOne("subscribeMapper.selectSubProduct", spno);
 	}
 }
