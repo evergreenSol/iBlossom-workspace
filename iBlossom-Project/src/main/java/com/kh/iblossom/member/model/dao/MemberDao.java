1ec54f6cf1cddb448668e6b8541925df13c8fbe9
evergreenSol <craft0429@gmail.com>
2022-07-07 18:29:09 +0900

    기타 작업
    기타 작업 - 한솔

--- iBlossom-Project/src/main/java/com/kh/iblossom/member/model/dao/MemberDao.java
+++ iBlossom-Project/src/main/java/com/kh/iblossom/member/model/dao/MemberDao.java
@@ -1,8 +1,6 @@
 package com.kh.iblossom.member.model.dao;
 
-import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.kh.iblossom.member.model.vo.Member;
